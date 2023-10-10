bc326c859773771d6340ca2b6158369f39bf0a68
Adjani <133295232+Adjani09@users.noreply.github.com>
2023-10-10 04:42:23 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -11,8 +11,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'User'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     userName = Column(String(250), nullable=False)
     firstName = Column(String(250), nullable=False)
@@ -23,6 +21,7 @@ class Post(Base):
     __tablename__ = 'Post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('User.id'))
+    user = relationship(User)
 
 class MyEnum(enum.Enum):
     photo = 1
@@ -35,21 +34,23 @@ class Media(Base):
     type = Column(Enum(MyEnum))
     url = Column(String(250), nullable=False)
     post_id = Column(Integer, ForeignKey('Post.id'))
+    post = relationship(Post)
     
 class Comment(Base):
     __tablename__ = 'Comment'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable=False)
     author_id = Column(Integer, ForeignKey('User.id'))
-    post_id = Column(Integer, ForeignKey('Post.id') )
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+    post = relationship(Post)
 
 class Follower(Base):
     __tablename__ = 'Follower'
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('User.id'))
     user_to_id = Column(Integer, ForeignKey('User.id'))
-
-
+    user = relationship(User)
 
 
     def to_dict(self):
